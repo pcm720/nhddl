0f7874611bd3427745d5e3847badd9c24ef8cecd
Ivan V <ivv@i1v.org>
2024-10-12 19:46:08 +0300

    module_init: handle loading external IRX modules properly (#3)

--- include/module_init.h
+++ include/module_init.h
@@ -1,19 +1,6 @@
 #ifndef _HDD_H_
 #define _HDD_H_
 
-// Macros for loading embedded IOP modules
-#define IRX_DEFINE(mod)                                                                                                                              \
-  extern unsigned char mod##_irx[] __attribute__((aligned(16)));                                                                                     \
-  extern unsigned int size_##mod##_irx
-
-#define IRX_LOAD(mod)                                                                                                                                \
-  logString("\tloading " #mod "\n");                                                                                                                 \
-  if (SifExecModuleBuffer(mod##_irx, size_##mod##_irx, 0, NULL, &iopret) < 0)                                                                        \
-    return ret;                                                                                                                                      \
-  if (iopret) {                                                                                                                                      \
-    return iopret;                                                                                                                                   \
-  }
-
 // Initializes Memory Card modules required to load HDD modules and neutrino ELF
 int init();
 
